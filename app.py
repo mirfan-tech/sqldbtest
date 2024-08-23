19c08cb54f89274f84c2325ed41e6459aa670a19
Mohammed Irfan <irfan_tech@hotmail.com>
2024-08-23 10:03:31 -0700

    sqlalachemy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
 import os
-import pyodbc
 
 # Load the environment variables from .env file
 load_dotenv()
@@ -9,17 +9,17 @@ load_dotenv()
 # Retrieve the connection string from the environment variable
 conn_str = os.getenv('SQLCONNSTR_MyDb')
 
-# Establish a connection to the SQL Server
-conn = pyodbc.connect(conn_str)
+# Initialize the SQLAlchemy engine
+engine = create_engine(f"mssql+pyodbc:///?odbc_connect={conn_str}", echo=True)
 
 # Initialize the Flask application
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    cursor = conn.cursor()
-    cursor.execute('SELECT sno, text FROM test')
-    rows = cursor.fetchall()
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT sno, text FROM test'))
+        rows = result.fetchall()
     
     # Pass the rows data to the template
     return render_template('index.html', rows=rows)
